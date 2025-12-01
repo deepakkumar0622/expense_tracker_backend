ff747c8a24592d455767c9345f2b5cb54e20255a
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-12-01 23:35:59 +0530

    Commit

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// for scheduling job
+// for scheduling job & for production only
 if (process.env.NODE_ENV === "production") job.start();
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
