cde2258c66e406b44fb51902624630637b683da7
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-12-01 23:25:54 +0530

    Commit

--- src/server.js
+++ src/server.js
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 import { initBD } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiting.js";
 import transactions from "./routes/transactions.js";
+import job from "./config/cron.js";
 
 // To get the env objects
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") job.start();
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 //To accept the json req/res body
 app.use(express.json());
